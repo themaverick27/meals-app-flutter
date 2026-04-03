5a346b6a0d1fc34c00a1ddefdc7a27bd48944fd8
themaverick27 <aniweshkumar04@gmail.com>
2026-04-03 12:31:37 +0530

    app main screen layout

---
+++ lib/screens/meals.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class Meals extends StatelessWidget {
+  const Meals({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        
+      ),
+    );
+  }
+}
